fd10909d99f06549588878102d0449b85c0f4b88
Gonçalo Martins <gmartins@isr.uc.pt>
2014-03-03 15:57:30 +0000

    .gitignore: Added command to ignore LZ4 sources we want to pull at compile time.
    mrgs_data_interface:
    Added command (on CMakeLists.txt) to pull LZ4 source files at compile time, as well as to link these to our executable.
    Started working on code related to network communication, namely callbacks.

--- mrgs_data_interface/src/data_interface_node.cpp
+++ mrgs_data_interface/src/data_interface_node.cpp
@@ -50,6 +50,10 @@
 #include "ros/ros.h"
 #include "mrgs_data_interface/ForeignMap.h"
 #include "mrgs_data_interface/ForeignMapVector.h"
+#include "mrgs_data_interface/NetworkMap.h"
+
+// LZ4 include:
+#include "lz4/lz4.h"
 
 // Other includes
 #include <string>
@@ -59,10 +63,43 @@
 #define MRGS_INTERFACE "eth0"
 
 // Global variables
-// To be written only once in main()
-std::string g_local_mac;
 // To be written only by the processMap callback
 nav_msgs::OccupancyGrid::ConstPtr g_latest_local_map;
+// To be written only by the newRobotInNetwork callback and once in main()
+// (at(0) is always our local mac)
+std::vector<std::string> g_peer_macs;
+// To be written by the processForeignMap callback
+std::vector<mrgs_data_interface::ForeignMap> g_foreign_map_vector;
+
+
+inline int getRobotID(std:: string mac){
+  // We're in trouble if the desired mac doesn't exist.
+  int i = 0;
+  for(i = 0; i < g_peer_macs.size() && g_peer_macs.at(i) != mac; i++) {}
+  if(i == g_peer_macs.size())
+    return -1; // not found
+  else
+    return i;
+}
+
+void processForeignMap(std::string ip, const mrgs_data_interface::NetworkMap::ConstPtr msg)
+{
+	ROS_DEBUG("Received a foreign map from %s.", ip.c_str());
+  /// Determine which robot sent the map (i.e. determine its ID)
+  int id = getRobotID(msg->mac);
+  
+  /// Decompress data
+  // Allocate and populate compressed buffer
+  char* compressed = new char[msg->compressed_data.size()];
+  for(int i = 0; i < msg->compressed_data.size(); i++)
+    compressed[i] = msg->compressed_data.at(i);
+  // Allocate decompression buffer
+  char* decompressed = new char [msg->decompressed_length];
+  // Decompress
+  int decompressed_bytes = LZ4_decompress_safe(compressed, decompressed, msg->compressed_data.size(), msg->decompressed_length);
+  
+  /// Copy data to foreign map vector
+}
 
 void processMap(const nav_msgs::OccupancyGrid::ConstPtr& map)
 {
@@ -74,22 +111,35 @@ int main(int argc, char **argv)
   // ROS init
   ros::init(argc, argv, "data_interface_node");
   ros::NodeHandle n;
-  ros::Subscriber map = n.subscribe<nav_msgs::OccupancyGrid>("map", 1, processMap);
   
   // Retrieve local MAC address
-  std::string mac_file_path = std::string("/sys/class/net/") + std::string(MRGS_INTERFACE) + std::string("/address");
+  std::string* mac_file_path = new std::string(std::string("/sys/class/net/") + std::string(MRGS_INTERFACE) + std::string("/address"));
+  std::string* local_mac = new std::string;
   std::ifstream mac_file;
-  mac_file.open(mac_file_path.c_str(), std::ios::in);
+  mac_file.open((*mac_file_path).c_str(), std::ios::in);
   if(mac_file.is_open())
   {
-    mac_file >> g_local_mac;
+    mac_file >> *local_mac;
     mac_file.close();
+    //g_peer_macs.push_back(*local_mac);
+    delete local_mac;
+    delete mac_file_path;
   }
-  else 
+  else
+  {
     ROS_ERROR("Can't open file mac address file!");
+    return -1;
+  }
+  
+  ROS_DEBUG("Got mac: %s.", g_peer_macs.at(0).c_str());
+  
+  ROS_INFO("Our local map is at index %d.", getRobotID(g_peer_macs.at(0)));
+  
+  // Declare callbacks
+  ros::Subscriber map = n.subscribe<nav_msgs::OccupancyGrid>("map", 1, processMap);
   
   // Regular execution: loop with spinOnce
-  ros::spin();
+  //ros::spin();
 
   return 0;
 }

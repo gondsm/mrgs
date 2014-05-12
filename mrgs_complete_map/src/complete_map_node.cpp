096a6e356f0c0418e6ccb29bd1ca3e1f8af777a3
Gonçalo Martins <gmartins@isr.uc.pt>
2014-05-12 10:41:57 +0100

    Made the quaternion multiplication an inline function.

--- mrgs_complete_map/src/complete_map_node.cpp
+++ mrgs_complete_map/src/complete_map_node.cpp
@@ -68,13 +68,9 @@ ros::ServiceClient g_client;
 ros::Publisher pub1;
 ros::Publisher pub2;
 
-geometry_msgs::Quaternion multiplyQuaternion(geometry_msgs::Quaternion q1, geometry_msgs::Quaternion q2)
+inline geometry_msgs::Quaternion multiplyQuaternion(geometry_msgs::Quaternion q1, geometry_msgs::Quaternion q2)
 {
   // This function multiplies two quaternions.
-  //(Q1 * Q2).w = (w1w2 - x1x2 - y1y2 - z1z2)
-  //(Q1 * Q2).x = (w1x2 + x1w2 + y1z2 - z1y2)
-  //(Q1 * Q2).y = (w1y2 - x1z2 + y1w2 + z1x2)
-  //(Q1 * Q2).z = (w1z2 + x1y2 - y1x2 + z1w2)
   geometry_msgs::Quaternion q3;
   q3.w = (q1.w*q2.w - q1.x*q2.x - q1.y*q2.y - q1.z*q2.z);
   q3.x = (q1.w*q2.x + q1.x*q2.w + q1.y*q2.z - q1.z*q2.y);

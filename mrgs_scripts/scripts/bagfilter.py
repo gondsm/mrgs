9e8bf9d3338ed1e8130d1a5754ba8746202a2eac
Gonçalo Martins <gmartins@isr.uc.pt>
2014-05-19 16:35:34 +0100

    Tweaked the launch file, made the script very very general.
    Both are now general. The launch file has the prefix as an argument,
    the script uses regular expressions to detect the correct topic.

--- mrgs_scripts/scripts/bagfilter.py
+++ mrgs_scripts/scripts/bagfilter.py
@@ -1,9 +1,22 @@
+# This script converts bagfiles that already existed in the project so that they may be played back into my system.
 import rosbag
+import re
 
-with rosbag.Bag('/home/vsantos/rosbag/2013-09-11-20-52-55_filtered.bag', 'w') as outbag:
-    for topic, msg, t in rosbag.Bag('/home/vsantos/host/2013-09-11-20-52-55.bag').read_messages():
-        if topic == "/robot_1/base_scan":
-            msg.header.frame_id = "/laser"
+# Configure here:
+input = '/home/vsantos/test/1.bag'
+output = '/home/vsantos/test/1_filtered.bag'
+#prefix_to_remove = "/robot_0"
+
+# Prefix is now removed via regular expression
+p = re.compile('/robot_./base_scan')
+
+# Process bagfile
+with rosbag.Bag(output, 'w') as outbag:
+    for topic, msg, t in rosbag.Bag(input).read_messages():
+        #if topic == prefix_to_remove + "/base_scan":
+        if p.match(topic):
+            #print('Detected! ' + topic)
+	    msg.header.frame_id = "/laser"
             outbag.write(topic, msg, t)
-        else:
-            outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
+        #else:
+            outbag.write(topic, msg, t)

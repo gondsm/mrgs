7960e1e02928e0e93693647d74850e60457ceb7d
Gonçalo Martins <gmartins@isr.uc.pt>
2014-06-24 19:21:58 +0100

    Built a simple version of the simple algorithm.
    This is only obstacle avoidance, for now.

--- mrgs_exploration/scripts/simple_exploration.py
+++ mrgs_exploration/scripts/simple_exploration.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
-# license removed for brevity
+
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
-pub = rospy.Publisher('cmd_vel', Twist)
-
-def laserCallback(scan):
-  return
+class SimpleExploration:
+  # Constructor
+  def __init__(self):
+    self.pub = rospy.Publisher('cmd_vel', Twist)
+    self.danger = False
+    self.danger_threshold = 0.1 # In meters
+  
+  # Callback for laser scans. This is where the magic happens.
+  def laserCallback(self, scan):
+    # Determine if we're in danger
+    for value in scan.ranges:
+      if value > scan.range_min and value < scan.range_max and value < danger_threshold:
+        self.danger = True
+        break
+    command = Twist()
+    
+    # Determine where we should go next, either forward or rotate
+    if self.danger == True:
+      command.angular.z = 0.2
+    else:
+      command.linear.x = 0.1
+      
+    # Publish command
+    self.pub.publish(command)
 
 if __name__ == '__main__':
   rospy.init_node('simple_exploration')
-  sub = rospy.Subscriber('scan', LaserScan, laserCallback)
-  command = Twist()
-  command.angular.z = 1
-  while(True):
-    pub.publish(command)
-    rospy.sleep(1.)
+  explore = SimpleExploration()
+  sub = rospy.Subscriber('scan', LaserScan, explore.laserCallback)
+
   rospy.spin()

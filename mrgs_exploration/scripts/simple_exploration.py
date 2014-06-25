edf4acaa52b86959634f11f8987bc1583d966b17
Gonçalo Martins <gmartins@isr.uc.pt>
2014-06-25 11:42:31 +0100

    Fixed a small syntax bug.

--- mrgs_exploration/scripts/simple_exploration.py
+++ mrgs_exploration/scripts/simple_exploration.py
@@ -63,7 +63,7 @@ class SimpleExploration:
     if self.danger == True:
       command.angular.z = turning_factor*self.angular_velocity
       rospy.loginfo("Publishing z = {}".format(command.angular.z))
-    elif self.turnin_away == True:
+    elif self.turn_away == True:
       command.linear.x = self.linear_velocity*(minimum_distance)
       command.angular.z = turning_factor*self.angular_velocity
       rospy.loginfo("Publishing x = {}, z = {}".format(command.linear.x, command.angular.z))

78a162596e4179c24ea937156d523ae499057135
Gonçalo Martins <gmartins@isr.uc.pt>
2014-06-25 17:28:45 +0100

    Tweaked the way speed varies with distances.

--- mrgs_scripts/scripts/simple_exploration.py
+++ mrgs_scripts/scripts/simple_exploration.py
@@ -72,10 +72,10 @@ class SimpleExploration:
         # Variable initialization
         self.danger = False
         self.turn_away = False
-        self.danger_threshold = 0.4 # In meters
-        self.turn_away_threshold = 0.7
-        self.angular_velocity = 0.2 # In m/s, somehow
-        self.linear_velocity = 1 # In m/s
+        self.danger_threshold = 0.4     # In meters
+        self.turn_away_threshold = 1.5  # Also meters
+        self.angular_velocity = 0.5     # In radians, maybe
+        self.linear_velocity = 1        # In m/s
         self.maximum_speed = 0.3
         self.minimum_speed = 0.01
         self.last_rotation_factor = 1
@@ -120,8 +120,8 @@ class SimpleExploration:
             command.angular.z = turning_factor*self.angular_velocity
             rospy.logdebug("Publishing z = {}".format(command.angular.z))
         elif self.turn_away == True:
-            command.linear.x = self.linear_velocity*(minimum_distance)/3
-            command.angular.z = turning_factor*self.angular_velocity*(2*minimum_distance)
+            command.linear.x = self.linear_velocity*(minimum_distance)/1.5
+            command.angular.z = turning_factor*self.angular_velocity/(minimum_distance)
             rospy.logdebug("Publishing x = {}, z = {}".format(command.linear.x, command.angular.z))
         else:
             command.linear.x = self.linear_velocity*(minimum_distance)
